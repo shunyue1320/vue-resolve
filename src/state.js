19a5476a36bffcfd4c980e07147daa5e2da8b16b
shunyue1320 <tsunyue1320@gmail.com>
2021-10-19 17:55:00 +0800

    vue-05 watch computed 实现

--- src/state.js
+++ src/state.js
@@ -1,35 +1,65 @@
 import { observe } from "./observe/index";
 
 export function initState(vm) {
-    const options = vm.$options
+  const options = vm.$options;
 
-    // 后续实现计算属性 、 watcher 、 props 、methods
-    if (options.data) {
-        initData(vm);
-    }
+  // 数据响应式
+  if (options.data) {
+    initData(vm);
+  }
+
+  // computed 响应式
+  if (options.computed) {
+    initComputed(vm);
+  }
+
+  // watcher 监听
+  if (options.watch) {
+    initWatch(vm);
+  }
 }
 
 function proxy(vm, source, key) {
-    Object.defineProperty(vm, key, {
-        get() {
-            return vm[source][key]
-        },
-        set(newValue) {
-            vm[source][key] = newValue;
-        }
-    })
+  Object.defineProperty(vm, key, {
+    get() {
+      return vm[source][key];
+    },
+    set(newValue) {
+      vm[source][key] = newValue;
+    },
+  });
 }
 
 function initData(vm) {
-    let data = vm.$options.data;
-    // 如果是函数就拿到函数的返回值 否则就直接采用data作为数据源
-    data = vm._data = typeof data === 'function' ? data.call(vm) : data
+  let data = vm.$options.data;
+  // 如果是函数就拿到函数的返回值 否则就直接采用data作为数据源
+  data = vm._data = typeof data === "function" ? data.call(vm) : data;
 
-    // 属性劫持 采用defineProperty将所有的属性进行劫持
+  // 属性劫持 采用defineProperty将所有的属性进行劫持
 
-    // 我期望用户可以直接通过 vm.xxx 获取值， 也可以这样取值 vm._data.xxx
-    for (let key in data) {
-        proxy(vm, '_data', key)
-    }
-    observe(data)
+  // 我期望用户可以直接通过 vm.xxx 获取值， 也可以这样取值 vm._data.xxx
+  for (let key in data) {
+    proxy(vm, "_data", key);
+  }
+  observe(data);
+}
+
+function initWatch(vm) {
+  const watch = vm.$options.watch;
+  for (const key in watch) {
+    let handler = watch[key]
+    // 对当前属性进行创建watcher，watcher中存放的回调是handler，取数据是从vm上获取
+    createWatcher(vm, key, handler)
+  }
+}
+
+
+function createWatcher(vm, key, handler) {
+  // handler 可以是对象
+  let options
+  if (typeof handler === 'object' && handler !== null) {
+    options = handler
+    handler = handler.handler
+  }
+  
 }
